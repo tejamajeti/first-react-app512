89cc32a14fdfccb68f3087c99b6a6ccff0be2e13
TEJA MAJETI <tejasaimanikanta2004@gmail.com>
2024-10-13 20:46:23 +0530

    initial commit

---
+++ src/components/Page/index.js
@@ -0,0 +1,12 @@
+import {Component} from "react"
+
+import "./index.css"
+
+class Page extends Component{
+    render(){
+        return <h1 className="my-page"> TEJA MAJETI </h1>
+    }
+}
+
+
+export default Page
